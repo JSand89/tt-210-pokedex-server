87e250ae0fb02086a1f8a9f7c5e06c559d799603
Jsand89 <javier.esand@gmail.com>
2024-10-28 07:49:39 -0500

    refactorizando pokemon statis

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const pokemonController = require("../controllers/pokemon")
+const pokemonStatusController = require("../controllers/pokemonStatus")
 const router = express.Router()
 
 router.get("/hello",pokemonController.hiTrainer)
 router.post("/",pokemonController.createPokemon)
 router.get("/",pokemonController.getPokemons)
 router.get("/:pokemon_id",pokemonController.getPokemonByIdPokemon)
-router.put("/view/:pokemon_id",pokemonController.viewPokemonById)
-router.put("/catch/:pokemon_id",pokemonController.catchPokemonById)
-router.put("/in_team/:pokemon_id",pokemonController.inTeamPokemonById)
+router.put("/view/:pokemon_id",pokemonStatusController.viewPokemonById)
+router.put("/catch/:pokemon_id",pokemonStatusController.catchPokemonById)
+router.put("/in_team/:pokemon_id",pokemonStatusController.inTeamPokemonById)
 module.exports = router
